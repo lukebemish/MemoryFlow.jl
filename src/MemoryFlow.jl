2a9356dd50de191960e2e8ea3b537c49b28ed247
Luke Bemish <lukebemish@gmail.com>
2021-10-08 23:00:35 -0500

    More complete version of Rozell, et al. algorithm
    The code for this algorithm seems to be mostly functional, though a lot
    more testing is needed. It should mostly work though

--- src/MemoryFlow.jl
+++ src/MemoryFlow.jl
@@ -1,26 +1,44 @@
 module MemoryFlow
-export SimpleNode, AbstractNode
-export solvediffeq, makediffeq, treeparse
+export make_nodes, eval_nodes, update_node
 
 using LSODA, DiffEqBase
 
 abstract type AbstractNode end
 
-struct SimpleNode <: AbstractNode
-    # we need checks for this function!
-    f::Function # f(this_node,input_nodes) -> ((t,x,xs) -> dx/dt)
+"""
+    struct SimpleNode <: AbstractNode
+        inputs::Vector
+        val::Real
+        starting_val::Real
+    end
+The bare minimum for a functional node. All nodes should be an AbstractNode.
+
+`inputs` is the Vector of input nodes.
+
+`val` is the current stored value of the node.
+
+`starting_val` is the value the node should start at during differential
+equation solving.
+"""
+mutable struct SimpleNode <: AbstractNode
     inputs::Vector
+    val::Float64
+    starting_val::Float64
+end
+
+function equation_constructor(this_node::AbstractNode,input_nodes)
+    (t,x,xs) -> 0
 end
 
 function treeparse(nodes::Vector, done::Vector)
     newdone=done
     for i=nodes
-        if i ∉ done
+        if i ∉ getproperty.(newdone,:first)
             outpair = i=>length(newdone)+1
-            newdone = [newdone;outpair]
+            push!(newdone,outpair)
         end
         for j=i.inputs
-            if j ∉ done
+            if j ∉ getproperty.(newdone,:first)
                 newdone = treeparse([j],newdone)
             end
         end
@@ -28,27 +46,48 @@ function treeparse(nodes::Vector, done::Vector)
     newdone
 end
 
-function makediffeq(parsedtree::Vector)
+function makediffeq(vectortree::Vector)
+    parsedtree = IdDict(vectortree)
     fs = []
-    for node=getproperty.(parsedtree,:first)
+    nodes = keys(parsedtree)
+    for node=nodes
         ids = [parsedtree[i] for i=node.inputs]
         idthis = parsedtree[node]
-        nf = node.f(node,node.inputs)
+        nf = equation_constructor(node,node.inputs)
         f = (t,x) -> nf(t,x[idthis],x[ids])
         push!(fs,f)
     end
     iterer = enumerate(fs)
-    return (dx,x,p,t) -> [dx[i]=f(t,x) for (i,f) in iterer], length(fs)
+    return (dx,x,p,t) -> [dx[i]=f(t,x) for (i,f) in iterer], nodes
 end
 
-function solvediffeq(neteq, size; x_init = 0.5, tmax=1.)
-    neteq! = diffeqtuple[1]
-    size = diffeqtuple[2]
+function solvediffeq(neteq!, nodes;tmax=1.)
     tspan = (0.,tmax)
-    x0 = fill(x_init,size)
+    x0 = [i.starting_val for i=nodes]
 
     prob = ODEProblem(neteq!,x0,tspan)
     solve(prob,lsoda())
+end
+
+function make_nodes end
+
+function update_val!(node::AbstractNode, val::Float64)
+    node.val = val
+end
+
+function eval_nodes(nodes::Vector;kwargs...)
+    tree = treeparse(nodes,[])
+    eq, ns = makediffeq(tree)
+    sol = solvediffeq(eq,ns;kwargs...)
+    last_sol = last(sol.u)
+    for (i,n)=enumerate(ns)
+        update_val!(n,last_sol[i])
+    end
+    return [i.val for i=nodes]
+end
+
+function update_node(node::AbstractNode)
+    # This does nothing; most nodes don't need updating.
 end
 
 include("nodes.jl")
--- src/nodes.jl
+++ src/nodes.jl
@@ -1,19 +1,48 @@
 using LinearAlgebra
 
-struct RozellLCANode <: AbstractNode
-    f::Function
+mutable struct InputNode <: AbstractNode
     inputs::Vector
+    val::Float64
+    starting_val::Float64
+end
+
+export set_input_val
+function set_input_val!(node::InputNode, val::Real)
+    node.starting_val=val
+    node.val=val
+end
+
+function make_nodes(type::Val{:Input},size::Integer)
+    [InputNode([],0,0) for i=1:size]
+end
+
+export set_input_val!
+
+function update_val!(node::InputNode, val::Float64)
+    # Do nothing
+end
+
+mutable struct RozellLCANode <: AbstractNode
+    inputs::Vector{AbstractNode}
+    val::Float64
+    starting_val::Float64
     competitive_key::Vector # vector of places in input where input is anti-hebbian
     biases::Vector # length is length(input) - length(competitive_key)
 end
 
-function rozell_lca_f_assembler(this_node,input_nodes)
+thresval = 0.2
+
+function rozell_thres(val)
+    (val-thresval)*(val>thresval)
+end
+
+function equation_constructor(this_node::RozellLCANode,input_nodes)
     ϕ = this_node.biases
     τ = 1
     Gs = [i.biases ⋅ ϕ for i=input_nodes[this_node.competitive_key]]
     in_ids = filter(x -> x ∉ this_node.competitive_key, 1:length(input_nodes))
     comp_iter = enumerate(this_node.competitive_key)
-    (t,x,xs) -> (ϕ ⋅ xs[in_ids] - x - sum([Gs[i]*xs[j] for (i,j)=comp_iter]))/τ
+    (t,x,xs) -> ((ϕ ⋅ xs[in_ids]) - x - sum([Gs[i]*rozell_thres(xs[j]) for (i,j)=comp_iter]))/τ
 end
 
 function make_rozell_lca_layer(size::Integer,inputs::Vector)
@@ -21,18 +50,29 @@ function make_rozell_lca_layer(size::Integer,inputs::Vector)
     for i=1:size
         init_biases = rand(Float64,length(inputs))
         normalize!(init_biases)
-        node = RozellLCANode(rozell_lca_f_assembler,inputs,[],init_biases)
+        node = RozellLCANode(
+            inputs,0,0,[],init_biases)
         push!(nodes,node)
     end
     for i=1:size
         to_add = filter(x->x!=nodes[i],nodes)
         push!(nodes[i].inputs,to_add...)
-        push!(nodes[i].competitive_key,length(inputs)+1:length(inputs)+size-1)
+        push!(nodes[i].competitive_key,(length(inputs)+1:length(inputs)+size-1)...)
     end
     nodes
 end
 
-export make_rozell_lca_layer
+function update_node(node::RozellLCANode)
+    # Node updating or Hebbian learning goes here
+end
+
+function make_nodes(type::Val{:RozellLCA},size::Integer,inputs::Vector,kwargs...)
+    return make_rozell_lca_layer(size,inputs;kwargs...)
+end
+
+function update_val!(node::RozellLCANode, val::Float64)
+    node.val = rozell_thres(val)
+end
 
 # This is an implementation based on the network described here:
 #
